78d2251ec6c954bbde635af7300e082ddc5a5fba
dilipsunar1216 <dilipsunar1216@gmail.com>
2024-02-26 09:35:06 +0530

    add my first project

---
+++ main.js
@@ -0,0 +1,10 @@
+// let array1 = ["hello parul", "khaskhfhk"];
+// console.log(array1)
+
+// setInterval(() => {
+//     array1.forEach((element) => {
+//         document.getElementById("welcome-text").innerHTML += element
+
+//     })
+    
+// }, 1000);
